db2c8f02f6098ee521a27a74d5b5c3ea1bc33bb7
happy.bhalodiya <happy.bhalodiya@brainvire.com>
2025-01-16 20:51:20 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Happy  Bhalodiya on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
